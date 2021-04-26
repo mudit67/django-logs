9761299373cb2722e4f1d15f8fe2d66435413f0d
mudit67 <bhardwajmudit006@gmail.com>
2021-04-26 12:04:58 +0530

    Migrations

--- articles/models.py
+++ articles/models.py
@@ -4,4 +4,4 @@ class Article(models.Model):
     title = models.CharField(max_length=100) 
     slug = models.SlugField()
     body = models.TextField()
-    date = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField(auto_now_add=True)
