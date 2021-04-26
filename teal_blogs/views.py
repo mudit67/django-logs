735f38c3f02a3d6a785e21db1d3c7c974e46ac02
mudit67 <bhardwajmudit006@gmail.com>
2021-04-26 11:44:17 +0530

    basic article app

--- teal_blogs/views.py
+++ teal_blogs/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def about(request): 
+def home(request): 
     # return HttpResponse("about page")
     return render(request, "home.html")
 
-def home(request): 
+
+
+def about(request): 
     # return HttpResponse("home page")
     return render(request, "about.html")
