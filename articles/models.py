391e19a429cca7c7586a6ee2da00293afd8f9345
mudit67 <bhardwajmudit006@gmail.com>
2023-05-16 12:59:18 +0530

    WiP

--- articles/models.py
+++ articles/models.py
@@ -5,3 +5,10 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+    def snippet(self):
+        return self.body[:50]
--- teal_blogs/urls.py
+++ teal_blogs/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('',views.home),
@@ -9,3 +10,6 @@ urlpatterns = [
     path('about/', views.about),
 
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
